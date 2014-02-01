0a5a0148eab0a888b90ecfed737c6a460090ebac
Alex <john.alexwelch@gmail.com>
2014-01-31 20:09:24 -0600

    Delete connectvars.php

--- Live/scripts/connectvars.php
+++
@@ -1,7 +0,0 @@
-<?php
-  // Define database connection constants
-  define('DB_HOST', 'rabbitracercom.ipagemysql.com');
-  define('DB_USER', 'radmin');
-  define('DB_PASSWORD', 'rabbit25');
-  define('DB_NAME', 'auction');
-?>
